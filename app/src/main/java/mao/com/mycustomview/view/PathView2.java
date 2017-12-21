80bdf2bf3f876d0007042fb8218946688a38acd7
mao <maoqitian068@163.com>
2017-12-21 17:45:43 +0800

    布尔运算示例练习

--- app/src/main/java/mao/com/mycustomview/view/PathView2.java
+++ app/src/main/java/mao/com/mycustomview/view/PathView2.java
@@ -55,7 +55,7 @@ public class PathView2 extends View {
         //demo1(canvas);
         //demo2(canvas);
         //demo3(canvas);
-
+        //demo4(canvas);
         //Path的布尔运算 画出太极的图示
         Path path1=new Path();
         Path path2=new Path();
@@ -83,7 +83,7 @@ public class PathView2 extends View {
         path6.addCircle(0,100,30, Path.Direction.CW);
 
         //三个色块的交集
-        path5.op(path3,Path.Op.INTERSECT);
+        path5.op(path3,Path.Op.INTERSECT);//太极白点
         canvas.drawPath(path5, mWhitePaint);
         canvas.drawPath(path6, mBlackPaint);
 
@@ -91,6 +91,53 @@ public class PathView2 extends View {
         canvas.drawPath(path7, mPaint);
     }
 
+    //布尔运算示例
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void demo4(Canvas canvas) {
+        int x=80;//圆的X坐标值
+        int r=100;//圆的半径
+        canvas.translate(250,0);
+        //画出两个圆，将两个圆的布尔值结果赋值到pathOpResult
+        Path path1 = new Path();
+        Path path2 = new Path();
+        mPaint.setTextSize(40);
+        Path pathOpResult = new Path();
+
+        path1.addCircle(-x,0,r, Path.Direction.CW);
+        path2.addCircle(x,0,r, Path.Direction.CW);
+
+        pathOpResult.op(path1,path2,Path.Op.DIFFERENCE);//差集
+        canvas.translate(0,200);
+        canvas.drawText("DIFFERENCE(差集)",240,0,mPaint);
+        canvas.drawText("Path1中减去Path2后剩下的部分",240,40,mPaint);
+        canvas.drawPath(pathOpResult,mPaint);
+
+        pathOpResult.op(path1,path2,Path.Op.REVERSE_DIFFERENCE);//差集
+        canvas.translate(0,300);
+        canvas.drawText("REVERSE_DIFFERENCE(差集)",240,0,mPaint);
+        canvas.drawText("Path2中减去Path1后剩下的部分",240,40,mPaint);
+        canvas.drawPath(pathOpResult,mPaint);
+
+        pathOpResult.op(path1,path2,Path.Op.INTERSECT);//交集
+        canvas.translate(0,300);
+        canvas.drawText("INTERSECT(交集)",240,0,mPaint);
+        canvas.drawText("DPath1与Path2相交的部分",240,40,mPaint);
+        canvas.drawPath(pathOpResult,mPaint);
+
+        pathOpResult.op(path1,path2,Path.Op.UNION);//并集
+        canvas.translate(0,300);
+        canvas.drawText("UNION(并集)",240,0,mPaint);
+        canvas.drawText("包含全部Path1和Path2",240,40,mPaint);
+        canvas.drawPath(pathOpResult,mPaint);
+
+        pathOpResult.op(path1,path2,Path.Op.XOR);//异或
+        canvas.translate(0,300);
+        canvas.drawText("XOR(异或)",240,0,mPaint);
+        canvas.drawText("包含Path1与Path2但不包括两者相交的部分",240,40,mPaint);
+        canvas.drawPath(pathOpResult,mPaint);
+    }
+
+    //Path填充模式为 非零环绕
     private void demo3(Canvas canvas) {
         Path path=new Path();
         // 添加小正方形 (通过这两行代码来控制小正方形边的方向,从而演示不同的效果)
@@ -101,7 +148,7 @@ public class PathView2 extends View {
         path.setFillType(Path.FillType.WINDING);//规则非零环绕
         canvas.drawPath(path,mPaint);
     }
-
+    //Path填充模式为 奇偶规则
     private void demo2(Canvas canvas) {
         mPaint.setStyle(Paint.Style.FILL);
         Path path=new Path();
